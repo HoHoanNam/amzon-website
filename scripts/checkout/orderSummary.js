594e9ea1544ccbd37ddbfefb81193d08b009e91b
Kaiyuko <m8mi0909@gmail.com>
2024-12-24 11:25:59 +0700

    Calculate delivery date with saturdays and sundays

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -8,8 +8,9 @@ import {
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -23,14 +24,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString = deliveryDate.format(
-      'dddd, MMMM D'
-    );
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container 
@@ -80,14 +74,8 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays,
-        'days'
-      );
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+
+      const dateString = calculateDeliveryDate(deliveryOption);
 
       const priceString = deliveryOption.priceCents === 0
         ? 'Free'
@@ -128,6 +116,7 @@ export function renderOrderSummary() {
         removeFromCart(productId);
 
         updateCartQuantity();
+        renderCheckoutHeader();
         renderOrderSummary();
         renderPaymentSummary();
       });
@@ -168,6 +157,8 @@ export function renderOrderSummary() {
         // technique is called an "early return".
 
         handleQuantityInput(productId);
+        renderCheckoutHeader();
+        renderOrderSummary();
         renderPaymentSummary();
       });
     });
@@ -179,13 +170,18 @@ export function renderOrderSummary() {
       input.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
           handleQuantityInput(productId);
+          renderCheckoutHeader();
+          renderOrderSummary();
+          renderPaymentSummary();
         }
-        renderPaymentSummary();
       })
     });
 
   //
   function handleQuantityInput(productId) {
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.remove('is-editing-quantity');
+
     const quantityInput = document
       .querySelector(`.js-quantity-input-${productId}`);
 
@@ -203,12 +199,15 @@ export function renderOrderSummary() {
 
     updateQuantity(productId, newQuantity);
 
-    const container = document.querySelector(`.js-cart-item-container-${productId}`);
-    container.classList.remove('is-editing-quantity');
-    const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
-    quantityLabel.innerHTML = newQuantity;
+    // We can delete the code below (from the original solution)
+    // because instead of using the DOM to update the page directly
+    // we can use MVC and re-render everything. This will make sure
+    // the page always matches the data.
 
-    updateCartQuantity();
+    // const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
+    // quantityLabel.innerHTML = newQuantity;
+
+    // updateCartQuantity();
   }
 
   document.querySelectorAll('.js-delivery-option')
